41705b5753fee2534c9c68ac17aec5d79c6957ad
cloudrohu <cloudrohu@gmail.com>
2026-08-28 00:39:31 +0530

    {% if settings_obj.logo  %}

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from apps.core.models import Setting
+
 
 def dashboard(request):
 
-    return render(request,"home/index.html")
+    settings_obj = Setting.objects.first()
+
+
+
+    return render(
+        request,
+        "home/index.html",
+        {
+            "settings_obj": settings_obj,
+
+
+        }
+    )
